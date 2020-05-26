6e6924e90802e5a4630571acd0c2ee39d847c4fd
satomi.araki <satomi@example.com>
2020-05-26 16:39:24 +0900

    apitoken change

--- slackbot_settings.py
+++ slackbot_settings.py
@@ -1,7 +1,5 @@
 #coding: utf-8
-import os
-# API_TOKEN = os.environ["API_TOKEN"]
-API_TOKEN = "xoxb-414832854468-1161150598752-MjJV2qlnOl6vXP6fBYOkrPqw"
+API_TOKEN = "xoxb-414832854468-1161150598752-QNua4ZlnzOhcRX1aTQVGzgxk"
 DEFAULT_REPLY = "Hey, I'm on Heroku!"
 
 PLUGINS = [
