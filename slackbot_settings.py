ef7119e78c75e52256dc51f29450b586053795b2
satomi.araki <satomi@example.com>
2020-05-26 16:02:26 +0900

    test apitoken direct writing

--- slackbot_settings.py
+++ slackbot_settings.py
@@ -1,6 +1,7 @@
 #coding: utf-8
 import os
-API_TOKEN = os.environ["API_TOKEN"]
+# API_TOKEN = os.environ["API_TOKEN"]
+API_TOKEN = "xoxb-414832854468-1161150598752-MjJV2qlnOl6vXP6fBYOkrPqw"
 DEFAULT_REPLY = "Hey, I'm on Heroku!"
 
 PLUGINS = [
