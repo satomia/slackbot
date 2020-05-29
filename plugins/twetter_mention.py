0496af2cce70ebc5db7bb36fa5abf3e823847854
satomi.araki <satomi@example.com>
2020-05-29 15:27:44 +0900

    add bot what's time?

---
+++ plugins/twetter_mention.py
@@ -0,0 +1,12 @@
+from slackbot.bot import Bot, respond_to, listen_to
+import re
+from datetime import datetime
+import locale
+
+locale.setlocale(locale.LC_CTYPE, "Japanese_Japan.932")
+
+# 「@bot名 今何時?」とメッセージすると時間を教えてくれる
+@respond_to('今何時')
+def now(message):
+    strftime = datetime.now().strftime("%Y/%m/%d %H:%M:%S")
+    message.reply(strftime)
